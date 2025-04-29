0da0f308323ab52bb573eb0880afedd9de4b3847
gaoxiaogang <husserl@qq.com>
2025-04-29 21:12:02 +0800

    填充ArrayQueue骨架，待实现

--- src/main/java/org/gxg/collection/ArrayQueue.java
+++ src/main/java/org/gxg/collection/ArrayQueue.java
@@ -1,4 +1,45 @@
 package org.gxg.collection;
 
-public class ArrayQueue {
+public class ArrayQueue<E> {
+    E[] a;
+    int N;
+    int first;// 指向队列头
+
+    @SuppressWarnings("unchecked")
+    public ArrayQueue() {
+        a = (E[]) new Object[1];
+        N = 0;
+        first = 0;
+    }
+
+    // 队列扩容
+    private void resize(int maxSize) {
+        // TODO
+    }
+
+    public void enqueue(E element) {
+        // TODO
+    }
+
+    // 从队列头取一个元素
+    public E dequeue() {
+        // TODO
+        E e = a[first];
+        return e;
+
+    }
+
+    public int size() {
+        // TODO
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        // TODO
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
