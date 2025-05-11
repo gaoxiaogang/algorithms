6979ef6e137262c86918f71b61b755cbaa60fe0c
gaoxiaogang <husserl@qq.com>
2025-05-11 11:00:19 +0800

    实现FixedCapacityStack类

--- src/main/java/org/gxg/collection/FixedCapacityStack.java
+++ src/main/java/org/gxg/collection/FixedCapacityStack.java
@@ -1,34 +1,73 @@
 package org.gxg.collection;
 
+import org.gxg.tools.In;
+import org.gxg.tools.StdIn;
+
 /**
  * 基于数组实现的固定容量通用栈
  */
-abstract public class FixedCapacityStack<Item> {
-    private Item[] a;// stack entries
+public class FixedCapacityStack<Item> {
+    private final Item[] a;// stack entries
 
     private int N;// size
 
+    @SuppressWarnings("unchecked")
     public FixedCapacityStack(int cap){
-        // TODO
-    };
+        a = (Item[]) new Object[cap];
+        N = 0;
+    }
 
     /**
      * 添加一个字符串
      */
-    abstract public void push(Item item);
+    public void push(Item item) {
+        if (N == a.length) {
+            throw new ArrayIndexOutOfBoundsException("Array a is full!");
+        }
+        a[N++] = item;
+    }
 
     /**
      * 删除最近添加的字符串
      */
-    abstract public Item pop();
+    public Item pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        Item item = a[--N];
+        a[N] = null;// TODO 这句代码是否可以去掉？
+        return item;
+    }
 
     /**
      * 栈是否为空
      */
-    abstract public boolean isEmpty();
+    public boolean isEmpty() {
+        return N == 0;
+    }
 
     /**
      * 栈中元素数量
      */
-    abstract public int size();
+    public int size() {
+        return N;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("请设置Stack容量：");
+        int cap = StdIn.readInt();
+        FixedCapacityStack<String> stk = new FixedCapacityStack<>(cap);
+
+        In in = new In("testData/stack_test.txt");
+        while (!in.isEmpty()) {
+            String item = in.readString();
+            if (item.equals("-")) {
+                System.out.print(stk.pop() + " ");
+            } else {
+                stk.push(item);
+            }
+        }
+        System.out.println(" ");
+        System.out.println("stack size: " + stk.size());
+    }
 }
