f7b2c2617074e1700dadbee25638936d67790491
gaoxiaogang <husserl@qq.com>
2025-05-11 12:17:59 +0800

    不依赖继承，直接实现ArrayStack类

--- src/main/java/org/gxg/collection/ArrayStack.java
+++ src/main/java/org/gxg/collection/ArrayStack.java
@@ -1,8 +1,88 @@
 package org.gxg.collection;
 
 import org.gxg.tools.In;
+import java.util.Iterator;
 
-public class ArrayStack<Item> extends ArrayAbstract<Item> {
+// 后进先出 LIFO (Last In, First Out)
+public class ArrayStack<Item> implements Iterable<Item> {
+    private Item[] Data;
+    private int N;// 元素数量
+
+    @SuppressWarnings("unchecked")
+    public ArrayStack() {
+        Data = (Item[]) new Object[1];
+        N = 0;
+    }
+
+    /**
+     * 扩大栈容量
+     * @param maxSize 扩大后的容量
+     */
+    @SuppressWarnings("unchecked")
+    private void resize(int maxSize) {
+        Item[] tmp = (Item[]) new Object[maxSize];
+        for (int i = 0; i < N; i++) {
+            tmp[i] = Data[i];
+        }
+        Data = tmp;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = N;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @Override
+        public Item next() {
+            return Data[--i];
+        }
+
+        @Override
+        public void remove() {}
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        if (N == Data.length) {
+            resize(2 * Data.length);// 扩容2倍
+        }
+        Data[N++] = item;
+    }
+
+    // 非标准方法，仅为展示扩容过程
+    public int capacity() {
+        return Data.length;
+    }
+
+    public Item pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        Item item = Data[--N];
+        Data[N] = null;
+
+        if (Data.length >= 4 && N == Data.length/4) {// 如果占用不足1/4，则缩容1倍
+            resize(Data.length/2);
+        }
+
+        return item;
+    }
+
     public static void main(String[] args) {
         ArrayStack<String> stk = new ArrayStack<>();
         In in = new In("testData/stack_test.txt");
