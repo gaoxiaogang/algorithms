6e410e18f66aa2e4c1fccf34dd729ae20ce8ffee
gaoxiaogang <husserl@qq.com>
2025-05-17 22:18:40 +0800

    完善基于readChar版本的ReversePolishNotation

--- src/main/java/org/gxg/app/stack/ReversePolishNotation.java
+++ src/main/java/org/gxg/app/stack/ReversePolishNotation.java
@@ -15,30 +15,42 @@ import java.util.Arrays;
  *
  */
 public class ReversePolishNotation {
-    // 判断一个字符串是否是一个数
-    private static boolean isNumeric(String str) {
-        return str != null && str.matches("^[-+]?\\d+(\\.\\d+)?([eE][-+]?\\d+)?$");
+    private static boolean isNumericChar(char c) {
+        return c >= '0' && c <= '9';
     }
     public static void main(String[] args) {
         ArrayStack<Double> vals = new ArrayStack<>();
         String file = "testData/app.stack_RPN.txt";
         In in = new In(file);
         while (!in.isEmpty()) {
-            String s = in.readString();
-            System.out.println("read s: [" + s + "]");
-            if (isNumeric(s)) {
-                vals.push(Double.parseDouble(s));
-                continue;
-            }
+            char c = in.readChar();
+            if (isNumericChar(c)) {// 遇到阿拉伯数字，继续往后找数字或小数点，组成一个完成的数后入栈
+                StringBuilder sb = new StringBuilder();
+                sb.append(c);
+                while (true) {
+                    char tmpChar = in.readChar();
+                    if (isNumericChar(tmpChar) || tmpChar == '.') {
+                        sb.append(tmpChar);
+                        continue;
+                    }
+
+                    c = tmpChar;
+                    break;// 跳出循环
+                }
+                Double d = Double.parseDouble(sb.toString());
+                vals.push(d);
+                System.out.println("read Double: " + d);
+            }// 数字入栈完毕
+
+            System.out.println("read s: [" + c + ']');
 
             // TODO 其他字符的处理逻辑
+            if (! (c == '+' || c == '-' || c == '*' || c == '/')) {
+                continue;// 非有效操作符，忽略
+            }
             double d1 = vals.pop();
             double d2 = vals.pop();
-            if (s.length() > 1) {
-                throw new RuntimeException("无效的操作符");
-            }
-            char op = s.charAt(0);
-            switch (op) {
+            switch (c) {
                 case '+':
                     vals.push(d2 + d1);
                     break;
@@ -52,12 +64,6 @@ public class ReversePolishNotation {
                     vals.push(d2 / d1);
                     break;
             }
-            /*
-            if (s.equals("+")) vals.push(d2 + d1);
-            else if (s.equals("-")) vals.push(d2 - d1);
-            else if (s.equals("*")) vals.push(d2 * d1);
-            else if (s.equals("/")) vals.push(d2 / d1);
-             */
         }
 
         System.out.println("vals.size: " + vals.size());
