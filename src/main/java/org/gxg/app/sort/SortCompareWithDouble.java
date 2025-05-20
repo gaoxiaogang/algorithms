fde17316ebb024484764fa84de1626449bce5cd7
gaoxiaogang <husserl@qq.com>
2025-05-20 10:18:54 +0800

    增加2个排序算法性能比较的用例：1个基于double数字，1个基于string

---
+++ src/main/java/org/gxg/app/sort/SortCompareWithDouble.java
@@ -0,0 +1,54 @@
+package org.gxg.app.sort;
+
+import org.gxg.sort.Insertion;
+import org.gxg.sort.Quick;
+import org.gxg.sort.Selection;
+import org.gxg.sort.Shell;
+import org.gxg.tools.StdRandom;
+import org.gxg.tools.Stopwatch;
+
+public class SortCompareWithDouble {
+
+    public static double time(String alg, Comparable[] a) {
+        Stopwatch timer = new Stopwatch();
+        if (alg.equals("Selection")) Selection.sort(a);
+        if (alg.equals("Insertion")) Insertion.sort(a);
+        if (alg.equals("Shell")) Shell.sort(a);
+        if (alg.equals("Quick")) Quick.sort(a);
+        return timer.elapsedTime();
+    }
+
+    private static Double[] randomDoubles(int n) {
+        Double a[] = new Double[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = StdRandom.uniformDouble();
+        }
+        return a;
+    }
+
+    public static double timeRandomInput(String alg, int N) {
+        return time(alg, randomDoubles(N));
+    }
+
+    public static void main(String[] args) {
+        String[] sorts = {
+                "Shell"
+                , "Selection"
+                , "Quick"
+                , "Insertion"
+        };
+//        String[] sorts = {"Quick"};
+        for(String alg : sorts) {
+            System.out.println("............sort name is: " + alg + "..........");
+            double time = timeRandomInput(alg, 1000);
+            System.out.printf("for %d random Doubles (%.2f seconds)\n", 1000, time);
+
+            time = timeRandomInput(alg, 10000);
+            System.out.printf("for %d random Doubles (%.2f seconds)\n", 10000, time);
+
+            time = timeRandomInput(alg, 100000);
+            System.out.printf("for %d random Doubles (%.2f seconds)\n", 100000, time);
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/org/gxg/app/sort/SortCompareWithString.java
@@ -0,0 +1,52 @@
+package org.gxg.app.sort;
+
+import org.gxg.sort.Insertion;
+import org.gxg.sort.Quick;
+import org.gxg.sort.Selection;
+import org.gxg.sort.Shell;
+import org.gxg.tools.In;
+import org.gxg.tools.StdRandom;
+import org.gxg.tools.Stopwatch;
+
+public class SortCompareWithString {
+
+    public static double time(String alg, Comparable[] a) {
+        Stopwatch timer = new Stopwatch();
+        if (alg.equals("Selection")) Selection.sort(a);
+        if (alg.equals("Insertion")) Insertion.sort(a);
+        if (alg.equals("Shell")) Shell.sort(a);
+        if (alg.equals("Quick")) Quick.sort(a);
+        return timer.elapsedTime();
+    }
+
+    private static String[] readWords() {
+        String file = "testData/sort_large_words.txt";
+//        String file = "testData/sort_words.shakespeare.txt";
+        In in = new In(file);
+        String[] a = in.readAllStrings();
+        in.close();
+        return a;
+    }
+
+    public static void main(String[] args) {
+        String[] sorts = {
+                "Shell"
+//                , "Selection"
+                , "Quick"
+                , "Insertion"
+        };
+//        String[] sorts = {"Quick"};
+
+        for(String alg : sorts) {
+            String[] a = readWords();
+            // 打乱顺序
+//            StdRandom.shuffle(a);
+
+            System.out.println("............sort name is: " + alg + "..........");
+            double time = time(alg, a);
+            System.out.printf("for %d random Strings (%.2f seconds)\n", a.length, time);
+
+            System.out.println();
+        }
+    }
+}
