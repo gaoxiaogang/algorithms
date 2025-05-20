878ce99303a404a39587648706fa2ba34c1fffe9
gaoxiaogang <husserl@qq.com>
2025-05-20 10:16:31 +0800

    待完善的双向队列

---
+++ src/main/java/org/gxg/collection/Deque.java
@@ -0,0 +1,40 @@
+package org.gxg.collection;
+
+import java.util.Iterator;
+
+// 双向队列
+public class Deque<E> implements Iterable<E> {
+    public Deque() {
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void pushLeft(E element) {
+
+    }
+
+    public void pushRight(E element) {
+
+    }
+
+    public E popLeft() {
+        return null;
+    }
+
+    public E popRight() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        // TODO
+        return null;
+    }
+}
