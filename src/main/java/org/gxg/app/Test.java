8c3164310ddc9e656762d2a5758dd316c4736da9
gaoxiaogang <husserl@qq.com>
2025-05-23 21:43:18 +0800

    追加测试代码

--- src/main/java/org/gxg/app/Test.java
+++ src/main/java/org/gxg/app/Test.java
@@ -2,9 +2,29 @@ package org.gxg.app;
 
 import org.gxg.collection.ArrayStack;
 import org.gxg.tools.In;
+import org.gxg.tools.Stopwatch;
 
 public class Test {
     public static void main(String[] args) {
+
+        // 实验结论：循环非常非常快，可以忽略这个成本
+        int n = 100000;
+        Integer[] arr = new Integer[n];
+        // 初始化
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+        Stopwatch timer = new Stopwatch();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp = arr[i];
+                arr[i] = arr[n-1];
+                arr[n-1] = tmp;
+            }
+        }
+        System.out.printf("foreach %d x %d times: %f", n, n, timer.elapsedTime());
+        System.out.println();
+
         ArrayStack<String> stk = new ArrayStack<>();
         In in = new In("testData/sort_words.txt");
         while (!in.isEmpty()) {
